5b50c1047fee23fbeec1f0626560c88fc54cd1a2
kabileshwaran <skabilesh231@gmail.com>
2025-08-01 22:17:54 +0530

    Add files via upload

---
+++ Day_4/Behavioral_level/jkflipflop/jk_flipflop.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module jk_flipflop(input j,k,clk,rst,
+                   output reg q);
+
+
+always @(posedge clk or posedge rst)begin
+ if(rst==1)
+    q<=0;
+    else begin   
+    case({j,k})
+    2'b00 : q=q;
+    2'b01 : q=0;
+    2'b10 : q=1;
+    2'b11 : q=~q;
+    endcase
+    end
+    end
+    
+endmodule
