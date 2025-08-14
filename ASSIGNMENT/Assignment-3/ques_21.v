847108f355ff4065a126f7a75c1ddc55a80ded91
kabileshwaran <skabilesh231@gmail.com>
2025-08-14 21:31:29 +0530

    Create ques_21.v

---
+++ ASSIGNMENT/Assignment-3/ques_21.v
@@ -0,0 +1,14 @@
+module nbit_4x1 #(
+    parameter N = 4)
+    (input  [N-1:0] d0, d1, d2, d3,
+    input  [1:0]   sel,            
+    output [N-1:0] y);
+
+  
+    assign y = (sel == 2'b00) ? d0 :
+               (sel == 2'b01) ? d1 :
+               (sel == 2'b10) ? d2 :
+                                 d3; 
+
+endmodule
+
