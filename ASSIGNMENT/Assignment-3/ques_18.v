4b263f0d1e1238908d21c232e3a216db1b4b55c7
kabileshwaran <skabilesh231@gmail.com>
2025-08-14 21:29:12 +0530

    Create ques_18.v

---
+++ ASSIGNMENT/Assignment-3/ques_18.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+module encoder_4x2ass(input[3:0]a,[1:0]y);
+  assign y=(a[0]==1)? 2'b00:
+    (a[1]==1)?2'b01:
+    (a[2]==1)?2'b10:
+    (a[3]==1)?2'b11:2'b00;
+    
+    
+endmodule
+
+
