7930f76cd995921ea03c161e37cbb106cc0f8a7b
kabileshwaran <skabilesh231@gmail.com>
2025-08-14 21:30:54 +0530

    Create ques_20.v

---
+++ ASSIGNMENT/Assignment-3/ques_20.v
@@ -0,0 +1,11 @@
+module demux_1to4 ( input  din,         
+    input  [1:0] sel,    
+    input  en,          
+    output [3:0] y );
+
+    assign y[0] = (en & (sel == 2'b00)) ? din : 1'b0;
+    assign y[1] = (en & (sel == 2'b01)) ? din : 1'b0;
+    assign y[2] = (en & (sel == 2'b10)) ? din : 1'b0;
+    assign y[3] = (en & (sel == 2'b11)) ? din : 1'b0;
+
+endmodule
