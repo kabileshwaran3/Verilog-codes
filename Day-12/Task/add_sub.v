55bd9c785235b1832cad69623228e614965d5920
kabileshwaran <skabilesh231@gmail.com>
2025-09-12 15:00:27 +0530

    Create add_sub.v

---
+++ Day-12/Task/add_sub.v
@@ -0,0 +1,33 @@
+module addition;
+  reg[7:0]a;
+  reg[7:0]b;
+  reg[7:0]sum;
+  reg[7:0]diff;
+  task add;
+    input[7:0]x;
+    input[7:0]y;
+    output[7:0]result;
+    begin
+      result=x+y;
+    end
+  endtask
+     
+    task sub;
+    input[7:0]x;
+    input[7:0]y;
+    output[7:0]result;
+    begin
+      result=x-y;
+    end
+     endtask
+    
+    initial begin
+      a=8'd15;
+      b=8'd10;
+      add(a,b,sum);
+      $display("sum=%d",sum);
+      sub(a,b,diff);
+      $display("diff=%d",diff);
+     
+    end
+    endmodule
