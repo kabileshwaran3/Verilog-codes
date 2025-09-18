e382191f75a3a7100991cd8b76e81d44a42cac97
kabileshwaran <skabilesh231@gmail.com>
2025-09-18 21:08:51 +0530

    Create ques_45.v

---
+++ ASSIGNMENT/Assignment-4/ques_45.v
@@ -0,0 +1,15 @@
+module mux_4to1 (
+    input  [3:0] data,   
+    input  [1:0] sel,    
+    output reg out );
+
+always @(*) begin
+    case (sel)
+        2'b00: out =data[0];
+        2'b01: out= data[1];
+        2'b10: out= data[2];
+        2'b11: out =data[3];
+        default: out = 1'b0;
+    endcase
+end
+endmodule
